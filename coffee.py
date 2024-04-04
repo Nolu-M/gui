adf62226b2d0a8b9ea085f77a77c54dfb8715fad
Nolubabalo Maxazana <nolubabalomaxazana@gmail.com>
2024-04-04 10:45:56 +0200

    added tea frame

--- coffee.py
+++ coffee.py
@@ -19,7 +19,6 @@ def open_coffee_options():
 
     my_canvas2.create_image(0,0, image=coffee_bg, anchor="nw")
     
-    
     # Add background image to the frame
     def resized(e):
         global coffee_bg, resized_coffee_bg, new_coffee_bg
@@ -37,7 +36,41 @@ def open_coffee_options():
     
     root.bind('<Configure>', resized)
 
+def open_tea_options():
+    # Hide the main menu buttons
+    button1.destroy()
+    button2.destroy()
+    button3.destroy()
     
+    # Create a new frame for coffee options
+    tea_frame = Frame(root)
+    tea_frame.place(relx=0.5, rely=0.5, anchor="center")
+    # define image
+    tea_bg = PhotoImage(file="images/tea.png")
+
+    # create a canvas
+    my_canvas3 = Canvas(tea_frame, width=1600, height=900)
+    my_canvas3.pack(fill="both", expand=True)
+
+    my_canvas3.create_image(0,0, image=tea_bg, anchor="nw")
+    
+    # Add background image to the frame
+    def resized(e):
+        global tea_bg, resized_tea_bg, new_tea_bg
+        # Open the image
+        tea_bg = Image.open("images/tea.png")
+        # Resize the image
+        resized_tea_bg = tea_bg.resize((1600, 900), Image.LANCZOS)
+        # Define image again
+        new_tea_bg = ImageTk.PhotoImage(resized_tea_bg)
+        # Add it back to the canvas
+        my_canvas3.create_image(0,0, image=new_tea_bg, anchor="nw")
+        # Read the text
+        my_canvas3.create_text(400, 100, text="ArdaCiti Coffee Shop", font=("Segoe Script", 40))
+        my_canvas3.create_text(400, 150, text="The Best In Town", font=("Segoe Script", 20))
+    
+    root.bind('<Configure>', resized)
+
 
 
 
@@ -56,7 +89,7 @@ my_canvas.pack(fill="both", expand=True)
 
 my_canvas.create_image(0,0, image=bg, anchor="nw")
 
-button1 = Button(text="Tea", bg="#8B3E2F", fg="white", pady=10, padx=30)
+button1 = Button(text="Tea", bg="#8B3E2F", fg="white", pady=10, padx=30, command=open_tea_options)
 button2 = Button(text="Coffee", bg="#8B3E2F", fg="white", pady=10, padx=30, command=open_coffee_options)
 button3 = Button(text="Voice Command", bg="#8B3E2F", fg="white", pady=10, padx=30)
 
